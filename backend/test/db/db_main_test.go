72fc4272f25954a494e0976fa7c7d43046a4a012
Ebenezer <eebenezer949@gmail.com>
2024-07-29 13:10:21 +0100

    test-db: establish connection with database

---
+++ backend/test/db/db_main_test.go
@@ -0,0 +1,28 @@
+package test
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+	db "github.com/ekefan/deal-tracker/internal/db/sqlc"
+)
+
+const (
+	dbDriver string = "postgres"
+	dbSource string = "postgresql://root:vasDealTracker@localhost:5432/dealTrackerDB?sslmode=disable"
+)
+
+var ts db.Store
+
+func TestMain(m *testing.M) {
+	dbConn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("couldn't connect to database")
+	}
+	ts = db.NewStore(dbConn)
+
+	os.Exit(m.Run())
+}
