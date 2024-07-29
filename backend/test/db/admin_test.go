a8b76c27338771760f4d1ac41a695457a2b93ca4
Ebenezer <eebenezer949@gmail.com>
2024-07-29 18:41:59 +0100

    test_db: create a new user

---
+++ backend/test/db/admin_test.go
@@ -0,0 +1,65 @@
+package test
+
+import (
+	"context"
+	"testing"
+
+	db "github.com/ekefan/deal-tracker/internal/db/sqlc"
+	util "github.com/ekefan/deal-tracker/test/util"
+	"github.com/stretchr/testify/require"
+)
+
+
+// createRandomUserArg generates the params need for creating a user
+func createRandomUserArg(role string) db.CreateNewUserParams {
+	return db.CreateNewUserParams{
+		Username: util.RandomString(5),
+		Role:     role,
+		FullName: util.GenFullname(),
+		Email:    util.GenEmail(),
+		Password: util.GenPassWord(),
+	}
+}
+
+// createNewUser loops through UsrRole and creates a user for each role
+func createNewUser(t *testing.T,  createUsers []UsrRole) []db.User {
+	 users := []db.User{}
+	for _, user := range createUsers {
+		t.Run(user.role, func(t *testing.T) {
+			arg := createRandomUserArg(user.role)
+			require.NotEmpty(t, arg)
+			newUsr, err := ts.CreateNewUser(context.Background(), arg)
+			require.NoError(t, err)
+			require.NotEmpty(t, newUsr)
+			require.NotEmpty(t, newUsr.ID)
+			require.NotEmpty(t, newUsr.CreatedAt)
+			require.Empty(t, newUsr.UpdatedAt)
+			require.False(t, newUsr.UpdatedAt.Valid)
+			require.Equal(t, newUsr.Role, arg.Role)
+			require.Equal(t, newUsr.Password, arg.Password)
+			require.Equal(t, newUsr.Email, arg.Email)
+			require.Equal(t, newUsr.FullName, arg.FullName)
+			users = append(users, newUsr)
+		})
+	}
+	return users
+}
+
+type UsrRole struct {
+	role string
+}
+
+
+func TestCreateNewUser(t *testing.T) {
+
+	createUsers := []UsrRole{
+		{role: "admin"},
+		{role: "manager"},
+		{role: "salesrep"},
+	}
+	
+	// Create a new User for each role
+	createNewUser(t, createUsers)
+}
+
+
