b6e251ed37d9d60bd15e7bbd0037cf51f52c0fe7
Ebenezer <eebenezer949@gmail.com>
2024-07-29 18:42:48 +0100

    test_db: create new_pitch_request

---
+++ backend/test/db/sale_rep_test.go
@@ -0,0 +1,41 @@
+package test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	db "github.com/ekefan/deal-tracker/internal/db/sqlc"
+	util "github.com/ekefan/deal-tracker/test/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createPitch(t *testing.T, salesRepId int64) db.PitchRequest {
+	
+	// args fields neeed to create a newPitch row
+	args := db.CreatePitchRequestParams{
+		SalesRepID:      salesRepId,
+		Status:          util.RandomString(4),
+		CustomerName:    util.GenFullname(),
+		PitchTag:        util.RandomString(3),
+		CustomerRequest: util.RandomString(6),
+		RequestDeadline: time.Now().UTC().Add(42 * time.Hour),
+	}
+	newPitch, err := ts.CreatePitchRequest(context.Background(), args)
+	require.NoError(t, err)
+	require.NotEmpty(t, newPitch)
+	require.NotEmpty(t, newPitch.ID)
+	require.NotEmpty(t, newPitch.CreatedAt)
+	require.Empty(t, newPitch.UpdatedAt)
+	require.False(t, newPitch.AdminViewed)
+	require.Equal(t, args.SalesRepID, newPitch.SalesRepID)
+	require.WithinDuration(t, args.RequestDeadline, newPitch.RequestDeadline, time.Second)
+	return newPitch
+}
+
+func TestCreatePitchRequest(t *testing.T) {
+	userRole := []UsrRole{{role: "salesRep"}}
+	salesRep := createNewUser(t, userRole)[0]
+	require.NotEmpty(t, salesRep)
+	createPitch(t, salesRep.ID)
+}
