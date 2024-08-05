bee1816f93287e306ed03a2af799024f7050152a
Ebenezer <eebenezer949@gmail.com>
2024-08-05 23:02:28 +0100

    server: handler for updating password when logged in or forgot password

---
+++ backend/internal/server/updatePassword.go
@@ -0,0 +1,112 @@
+// On login for !password_changed
+// createnewPasswordEndpoint
+
+/////////ON FORGOT PASSWORD////////////////////
+// create a new password, save hash and update the database....
+// send user created Password in the email....
+// send user createnewPasswordendpoint.....
+// send user to createnewPassword endpoint: // receives one update
+
+
+// ///CREATE FORGOTPASSWORDENDPOINT
+// request carries user email,
+package server
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"time"
+
+	db "github.com/ekefan/deal-tracker/internal/db/sqlc"
+	"github.com/ekefan/deal-tracker/internal/utils"
+	"github.com/gin-gonic/gin"
+)
+
+// / this handler would be called after a use logs in and their password_changed status is false
+type UpdatePasswordReq struct {
+	OldPassword string `json:"old_password" binding:"required"`
+	Password    string `json:"password_update" binding:"required,min=6"`
+	UserID      int64  `json:"user_id"`
+}
+
+
+// updatePassWordLoggedIn takes the current password then updates the user password
+func (s *Server) updatePassWordLoggedIn(ctx *gin.Context) {
+	var req UpdatePasswordReq
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	//maybe using session management.. check if user is loggedin
+	usr, err := s.Store.GetUserForUpdate(ctx, req.UserID)
+	if err != nil {
+		if sqlNoRowsHandler(ctx, err) {
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+	if !utils.CheckPasswordHash(req.OldPassword, usr.Password) {
+		ctx.JSON(http.StatusUnauthorized, errorResponse(fmt.Errorf("password invalid: %v", err)))
+		return
+	}
+	success := s.Store.UpdatePassWord(ctx, db.UpdatePassWordParams{
+		ID:       req.UserID,
+		Password: req.Password,
+		PasswordChanged: true,
+		UpdatedAt: sql.NullTime{
+			Time:  time.Now(),
+			Valid: true,
+		}, 
+	})
+	if success != nil {
+		if sqlNoRowsHandler(ctx, err) {
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"mesaage": "succesful",
+	})
+}
+
+
+type ForgotPasswordReq struct {
+	Email string `json:"email" binding:"required,email"`
+}
+func (s *Server) forgotPassword(ctx *gin.Context) {
+	var req ForgotPasswordReq
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	usr, err := s.Store.ForgotPassword(ctx, req.Email)
+	if err != nil {
+		if sqlNoRowsHandler(ctx, err) {
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	newRandomPass := randomPasswordCode()
+	updatePasswordArgs := db.UpdatePassWordParams{
+		ID: usr.ID,
+		Password: newRandomPass,
+		PasswordChanged: true,
+		UpdatedAt: sql.NullTime{
+			Time: time.Now(),
+			Valid: true,
+		},
+	}
+	success := s.Store.UpdatePassWord(ctx, updatePasswordArgs)
+	if success != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	//send user the newPassword to their email....
+	ctx.JSON(http.StatusOK, gin.H{
+		"mesaage": "succesful",
+	})
+}
