29451efeca41800bf74b1375d1854337ccb77bac
Ebenezer <eebenezer949@gmail.com>
2024-08-10 09:37:43 +0100

    token: paseto maker for access tokens

---
+++ backend/internal/token/paseto.go
@@ -0,0 +1,57 @@
+package token
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/aead/chacha20poly1305"
+	"github.com/o1egl/paseto"
+)
+
+type TokenMaker interface {
+	CreateToken(username, role string, duration time.Duration) (string, error)
+	VerifyToken(token string)(*Payload, error)
+}
+
+type PasetoMaker struct {
+	paseto       *paseto.V2
+	symmetricKey []byte
+}
+
+// Create's a new paseto maker instance...
+func NewPaseto(symmetricKey string) (*PasetoMaker, error) {
+	if len(symmetricKey) != chacha20poly1305.KeySize {
+		return nil, fmt.Errorf("SymmetricKey too short should be: %v", chacha20poly1305.KeySize)
+	}
+
+	maker := &PasetoMaker{
+		paseto:       paseto.NewV2(),
+		symmetricKey: []byte(symmetricKey),
+	}
+
+	return maker, nil
+}
+
+// / CreateToken takes username, role and duration to create an access token
+func (maker *PasetoMaker) CreateToken(username, role string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, role, duration)
+	if err != nil {
+		return "", err
+	}
+	return maker.paseto.Encrypt(maker.symmetricKey, payload, nil)
+}
+
+func (maker *PasetoMaker) VerifyToken(token string) (*Payload, error) {
+	payload := &Payload{}
+	err := maker.paseto.Decrypt(token, maker.symmetricKey, payload, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	err = payload.Valid()
+	if err != nil {
+		return nil, err
+	}
+
+	return payload, nil
+}
