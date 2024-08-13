cd7d8f359211fd5b992010e4953cbe199aac0bc3
Ebenezer <eebenezer949@gmail.com>
2024-08-13 14:01:51 +0100

    db solve pq error with name undefined function

--- backend/internal/db/queries/userAndDeals.sql
+++ backend/internal/db/queries/userAndDeals.sql
@@ -1,49 +1,30 @@
 -- name: FilterDeals :many
-WITH params AS (
-    SELECT
-        $1::TEXT AS customer_name,
-        $2::TEXT AS service_to_render,
-        $3::TEXT AS status,
-        $4::NUMERIC AS min_profit,
-        $5::NUMERIC AS max_profit,
-        $6::BOOLEAN AS awarded,
-        $7::TEXT AS sales_rep_name
-)
-SELECT * FROM deals
+SELECT id, pitch_id, sales_rep_name, customer_name, service_to_render, status, status_tag, current_pitch_request, net_total_cost, profit, created_at, updated_at, closed_at, awarded
+FROM deals
 WHERE 
-    (customer_name = (SELECT customer_name FROM params) OR (SELECT customer_name FROM params) IS NULL) AND
-    (service_to_render = (SELECT service_to_render FROM params) OR (SELECT service_to_render FROM params) IS NULL) AND
-    (status = (SELECT status FROM params) OR (SELECT status FROM params) IS NULL) AND
-    (profit >= (SELECT min_profit FROM params) OR (SELECT min_profit FROM params) IS NULL) AND
-    (profit <= (SELECT max_profit FROM params) OR (SELECT max_profit FROM params) IS NULL) AND
-    (awarded = (SELECT awarded FROM params) OR (SELECT awarded FROM params) IS NULL) AND
-    (sales_rep_name = (SELECT sales_rep_name FROM params) OR (SELECT sales_rep_name FROM params) IS NULL)
+    (customer_name = $1 OR $1 IS NULL) AND
+    (service_to_render && $2::TEXT[] OR $2 IS NULL) AND
+    (status = $3 OR $3 IS NULL) AND
+    (profit >= $4 OR $4 IS NULL) AND
+    (profit <= $5 OR $5 IS NULL) AND
+    (awarded = $6 OR $6 IS NULL) AND
+    (sales_rep_name = $7 OR $7 IS NULL)
 ORDER BY id
 LIMIT $8
 OFFSET $9;
 
 
 -- name: CountFilteredDeals :one
-WITH params AS (
-    SELECT
-        $1::TEXT AS customer_name,
-        $2::TEXT AS service_to_render,
-        $3::TEXT AS status,
-        $4::NUMERIC AS min_profit,
-        $5::NUMERIC AS max_profit,
-        $6::BOOLEAN AS awarded,
-        $7::TEXT AS sales_rep_name
-)
-SELECT COUNT(*) FROM deals
+SELECT COUNT(*)
+FROM deals
 WHERE 
-    (customer_name = (SELECT customer_name FROM params) OR (SELECT customer_name FROM params) IS NULL) AND
-    (service_to_render = (SELECT service_to_render FROM params) OR (SELECT service_to_render FROM params) IS NULL) AND
-    (status = (SELECT status FROM params) OR (SELECT status FROM params) IS NULL) AND
-    (profit >= (SELECT min_profit FROM params) OR (SELECT min_profit FROM params) IS NULL) AND
-    (profit <= (SELECT max_profit FROM params) OR (SELECT max_profit FROM params) IS NULL) AND
-    (awarded = (SELECT awarded FROM params) OR (SELECT awarded FROM params) IS NULL) AND
-    (sales_rep_name = (SELECT sales_rep_name FROM params) OR (SELECT sales_rep_name FROM params) IS NULL);
-
+    (customer_name = $1 OR $1 IS NULL) AND
+    (service_to_render && $2::TEXT[] OR $2 IS NULL) AND
+    (status = $3 OR $3 IS NULL) AND
+    (profit >= $4 OR $4 IS NULL) AND
+    (profit <= $5 OR $5 IS NULL) AND
+    (awarded = $6 OR $6 IS NULL) AND
+    (sales_rep_name = $7 OR $7 IS NULL);
 
 -- name: GetDealsByStatus :many
 SELECT * FROM deals
