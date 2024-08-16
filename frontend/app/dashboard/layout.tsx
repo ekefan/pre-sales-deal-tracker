350ade376ca08007bdac90728bbdcdcfc0d148d4
Ebenezer <eebenezer949@gmail.com>
2024-08-16 22:09:29 +0100

    dashboard: layout holding side nav and main

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -1,10 +1,11 @@
+import SideNav from "@/components/main/sidenav";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className="bg-slate-200 flex w-full h-screen md:w-11/12 md:rounded-r-2xl items-center p-2">
         <div className="bg-slate-100 w-full h-full rounded md:absolute md:right-0 md:h-5/6 md:rounded-l-2xl md:w-11/12 flex gap-2 p-3 flex-col md:flex-row">
-          <div className="bg-slate-50 rounded md:rounded-2xl">SideNav</div>
-          <main className="bg-slate-50 rounded flex-grow md:rounded-2xl">{children}</main>
+          <div className="md:rounded-2xl md:w-1/6 flex flex-col gap-2"><SideNav/></div>
+          <main className="bg-slate-50 rounded flex-grow">{children}</main>
         </div>
       </div>
     </>
