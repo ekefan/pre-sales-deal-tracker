ce91c85f20228bd05e77f597678484140326086e
Ebenezer <eebenezer949@gmail.com>
2024-08-17 06:19:49 +0100

    Navigation: styled the logo/title, completed

--- frontend/components/main/sidenav.tsx
+++ frontend/components/main/sidenav.tsx
@@ -3,7 +3,7 @@ import NavLinks from "./navLinks";
 export default function SideNav() {
   return (
     <>
-      <div className="bg-slate-50 h-14 md:h-16 rounded flex items-end p-2">VDT</div>
+      <div className="bg-sky-100 h-14 md:h-16 rounded flex items-end justify-start pt-3 pl-3 pb-1 font-medium text-sky-600 text-base md:text-lg">Vas Deal Tracker</div>
       <div className="bg-slate-50 h-8 grow flex space-x-2 md:space-x-0 md:space-y-2 justify-between items-center md:flex-col p-2 rounded"><NavLinks user=""/></div>
     </>
   );
