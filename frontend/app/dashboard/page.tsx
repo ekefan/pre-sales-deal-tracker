fa72aef028cd0e10b1e496a143191af209ea1b04
Ebenezer <eebenezer949@gmail.com>
2024-08-18 10:01:27 +0100

    Dashboard: home page... awaiting card design

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -4,20 +4,16 @@ export default async function Page() {
   const dateString = newDate.toDateString();
 
   return (
-    <div className="flex flex-col gap-4 pb-2 px-2 h-full w-full relative">
-      <div className="flex flex-col p-2 bg-slate-50 gap-4">
+    <div className="flex flex-col pb-2 px-2 h-full w-full relative">
+      <div className="flex flex-col p-3 bg-slate-50 gap-4 sm:text-base md:text-lg text-slate-700">
         <div>
-          <p>Welcome {`<first name>`}</p>
-          <p>{dateString}</p>
+          <p className="font-medium">Welcome {`<first name>`}</p>
+          <p className="text-sm md:text-base">{dateString}</p>
         </div>
-        <p>Ongoing Deals</p>
+        <p className="text-slate-600">Ongoing Deals</p>
       </div>
-      <div className="flex flex-col gap-3  p-2 w-full grow h-auto">
+      <div className="flex flex-col gap-3  p-3 w-full grow h-auto">
         <section className="flex flex-col w-full h-full gap-3 ">
-          <div className="bg-green-200 w-full h-32 rounded-lg border"></div>
-          <div className="bg-yellow-200 w-full h-32 rounded-lg border"></div>
-          <div className="bg-indigo-300 w-full h-32 rounded-lg border"></div>
-          <div className="bg-pink-300 w-full h-32 rounded-lg border"></div>
           <div className="bg-green-200 w-full h-32 rounded-lg border"></div>
           <div className="bg-yellow-200 w-full h-32 rounded-lg border"></div>
           <div className="bg-indigo-300 w-full h-32 rounded-lg border"></div>
