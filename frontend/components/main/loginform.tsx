ae2446b9ab30f3fa64b127b0e748f52bd1556df5
Ebenezer <eebenezer949@gmail.com>
2024-08-20 12:16:49 +0100

    Login: login form

---
+++ frontend/components/main/loginform.tsx
@@ -0,0 +1,32 @@
+export default function LoginForm() {
+  return (
+    <form action="">
+    <div className="p-3 border rounded-xl flex flex-col gap-4">
+      <div className="border rounded p-1 flex gap-2">
+        <label htmlFor="email">email:</label>
+        <input
+          type="text"
+          placeholder="example@gmail.com"
+          className="outline-none"
+        />
+      </div>
+      <div>
+        <div className="border rounded p-1 flex gap-2">
+          <label htmlFor="password">password:</label>
+          <input
+            type="password"
+            className="outline-none"
+            name=""
+            id=""
+            aria-describedby="helpId"
+            placeholder="password"
+          />
+        </div>
+      </div>
+      <div className="w-full flex justify-center items-center">
+        <button className="border rounded-md w-1/4 ">login</button>
+      </div>
+    </div>
+  </form>
+  );
+}
