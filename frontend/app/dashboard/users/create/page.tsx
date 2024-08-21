31f1aba1ec441f767177cef1691b9dc07e65a24b
Ebenezer <eebenezer949@gmail.com>
2024-08-21 08:55:57 +0100

    Users: create user form

--- frontend/app/dashboard/users/create/page.tsx
+++ frontend/app/dashboard/users/create/page.tsx
@@ -1,3 +1,11 @@
+import { CreateUserForm } from "@/components/users/createUserForm";
 export default function Home() {
-    return <>create user</>
+  return (
+    <div className="flex  bg-slate-50 text-sm md:text-base font-normal flex-col p-2 h-auto w-full sm:w-5/6 md:w-10/12 lg:w-8/12 xl:w-6/12 relative">
+      <div className="p-3">Create User</div>
+      <div className="p-3 mt-1 flex flex-col gap-3 w-full rounded-md bg-gray-100">
+        <CreateUserForm />
+      </div>
+    </div>
+  );
 }
