ac1bb42d53bbd5627fd69ac87f5af36f2a1cff39
Ebenezer <eebenezer949@gmail.com>
2024-08-22 10:57:16 +0100

    Dashboard: profile update

--- frontend/app/dashboard/profile/update/page.tsx
+++ frontend/app/dashboard/profile/update/page.tsx
@@ -1,3 +1,25 @@
+import { UpdateProfileForm } from "@/components/profile/updateProfile";
 export default function Home() {
-    return <>update profile</>
+  //get user;
+  const usr = {
+    userId: 9,
+    fullname: "Lily Joshua",
+    username: "lily",
+    email: "eebenezer949@gmail.com",
+  };
+  return (
+    <div className="flex  bg-slate-50 text-sm md:text-base font-normal flex-col p-2 h-auto w-full sm:w-5/6 md:w-10/12 lg:w-8/12 xl:w-6/12 relative">
+      <div className="p-3">Update profile</div>
+      <div className="p-3">
+        <div className="p-2 flex flex-col gap-3 w-full rounded-md bg-gray-100">
+          <UpdateProfileForm
+            userId={usr.userId}
+            email={usr.email}
+            username={usr.username}
+            fullname={usr.fullname}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
