a14c5b680314e54ed2dde99e3da93fdc6d227731
Ebenezer <eebenezer949@gmail.com>
2024-08-25 22:48:51 +0100

    Components: split login form to handle submission on the backend

--- frontend/components/main/loginform.tsx
+++ frontend/components/main/loginform.tsx
@@ -1,34 +1,91 @@
+"use client";
 import Link from "next/link";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import axios from "axios"; // Import axios
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  username: z.string().min(1, { message: "username is required" }),
+  password: z.string().min(6),
+});
+
+interface LoginResp {
+  user_id: string;
+  username: string;
+  role: string;
+  fullname: string;
+  email: string;
+  updatedAt: number;
+  createdAt: number;
+}
 
 export default function LoginForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      const response = await axios.post<LoginResp>('/api/login', {
+        username: values.username,
+        password: values.password,
+      });
+
+      console.log("Response Status:", response.status);
+      const user = response.data;
+      console.log(user);
+    } catch (error) {
+      console.log("Login error:", error);
+    }
+    console.log(values);
+  }
   return (
-    <form action="">
-    <div className="p-3 border rounded-xl flex flex-col gap-4">
-      <div className="border rounded p-1 flex gap-2">
-        <label htmlFor="email">email:</label>
-        <input
-          type="text"
-          placeholder="example@gmail.com"
-          className="outline-none"
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter your username" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
         />
-      </div>
-      <div>
-        <div className="border rounded p-1 flex gap-2">
-          <label htmlFor="password">password:</label>
-          <input
-            type="password"
-            className="outline-none"
-            name=""
-            id=""
-            aria-describedby="helpId"
-            placeholder="password"
-          />
-        </div>
-      </div>
-      <div className="w-full flex justify-center items-center">
-        <Link href="/dashboard" className="border flex items-center justify-center rounded-md w-1/4 ">login</Link>
-      </div>
-    </div>
-  </form>
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="Input your password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   );
 }
