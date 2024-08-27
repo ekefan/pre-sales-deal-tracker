2a750e535a81d6e9c159ae8e930611f5117552e6
Ebenezer <eebenezer949@gmail.com>
2024-08-27 16:05:11 +0100

    Receive user at dashboard: send request to back

--- frontend/app/api/login/route.ts
+++ frontend/app/api/login/route.ts
@@ -1,12 +1,10 @@
-// app/api/login/route.ts
 import { NextResponse } from 'next/server';
 import axios from 'axios';
 
 export async function POST(request: Request) {
   try {
     const { username, password } = await request.json();
-    console.log("program got here too", username, password)
-    const response = await axios({
+        const response = await axios({
         method: "post",
         baseURL: "http://localhost:8080",
         url:"/users/login",
