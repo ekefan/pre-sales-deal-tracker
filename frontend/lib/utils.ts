ca4f1adda3838f060113191d2f5297479baa1bd8
Ebenezer <eebenezer949@gmail.com>
2024-08-28 23:30:32 +0100

    Lib: define consts

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,6 +1,10 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-
+import axios from "axios"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export const DEFAULT_PAGE_SIZE: number = 20;
+export const ADMIN_ROLE: string = "admin";
