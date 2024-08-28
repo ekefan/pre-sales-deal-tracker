06dda007b262c49b0b77e133cde9570169deb4f8
Ebenezer <eebenezer949@gmail.com>
2024-08-28 23:23:03 +0100

    Deal Provider: create deal provider for card section

--- frontend/app/dashboard/deals/page.tsx
+++ frontend/app/dashboard/deals/page.tsx
@@ -5,36 +5,29 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import Link from "next/link";
-import { DealCard } from "@/components/deals/card";
-export default function Page() {
+import { CardSection } from "@/components/deals/cardSection";
+import { DealProvider } from "@/context/dealContext";
+export default async function Page() {
   return (
     <>
       <div className="flex flex-col text-sm md:text-base p-2 h-full w-full xl:w-11/12">
         <div className="p-3">Explore Deals</div>
-        <div className="flex justify-end px-3 py-1">
-          <Popover>
-            <PopoverTrigger className="w-20 sm:w-24 md:w-28 lg:w-32">
-              <div className="flex border p-2 justify-between gap-2 rounded-lg items-center bg-slate-800 hover:bg-sky-200 hover:text-slate-900 text-white">
-                <div className=" ">Filter</div>
-                <SlidersHorizontal size={18} />
-              </div>
-            </PopoverTrigger>
-            <PopoverContent className="w-full">
-              <FilterDealsForm />
-            </PopoverContent>
-          </Popover>
-        </div>
-        <div className="flex flex-col gap-3  p-3 w-full grow h-auto">
-          <section className="flex flex-col w-full h-full gap-3 ">
-            <div className="bg-green-200 w-full h-32 rounded-lg border flex justify-end items-center">
- 
-            </div>
-            <DealCard />
-            <div className="bg-indigo-300 w-full h-32 rounded-lg border"></div>
-            <div className="bg-pink-300 w-full h-32 rounded-lg border"></div>
-          </section>
-        </div>
+        <DealProvider>
+          <div className="flex justify-end px-3 py-1">
+            <Popover>
+              <PopoverTrigger className="w-20 sm:w-24 md:w-28 lg:w-32">
+                <div className="flex border p-2 justify-between gap-2 rounded-lg items-center bg-slate-800 hover:bg-sky-200 hover:text-slate-900 text-white">
+                  <div className=" ">Filter</div>
+                  <SlidersHorizontal size={18} />
+                </div>
+              </PopoverTrigger>
+              <PopoverContent className="w-full">
+                <FilterDealsForm />
+              </PopoverContent>
+            </Popover>
+          </div>
+          <CardSection page="deals" />
+        </DealProvider>
       </div>
     </>
   );
