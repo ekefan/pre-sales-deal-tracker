c92966ee15e17f132f38c6ec216c3dc85bbac71d
Ebenezer <eebenezer949@gmail.com>
2024-09-30 15:56:24 +0100

    feat: init http server for deal tracker api

---
+++ backend/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func main() {
+	dbpool, err := pgxpool.New(context.Background(), "postgresql://root:vasDealTracker@localhost:5432/dealTrackerDB?sslmode=disable")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+	defer dbpool.Close()
+
+	fmt.Printf("%T\n", dbpool)
+}
