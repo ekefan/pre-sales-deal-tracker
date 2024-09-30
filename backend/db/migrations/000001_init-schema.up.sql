fe3a78bee9ad1385270538ffc056e92e203db42d
Ebenezer <eebenezer949@gmail.com>
2024-09-30 15:53:00 +0100

    db: create db migrations

---
+++ backend/db/migrations/000001_init-schema.up.sql
@@ -0,0 +1,43 @@
+CREATE TABLE "users" (
+  "id" bigserial PRIMARY KEY,
+  "username" varchar UNIQUE NOT NULL,
+  "role" varchar NOT NULL,
+  "full_name" varchar NOT NULL,
+  "email" varchar UNIQUE NOT NULL,
+  "password" varchar NOT NULL,
+  "password_changed" bool NOT NULL DEFAULT false,
+  "updated_at" timestamp NOT NULL DEFAULT ('0001-01-01 00:00:00'),
+  "created_at" timestamp NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "deals" (
+  "id" bigserial PRIMARY KEY,
+  "pitch_id" bigint UNIQUE,
+  "sales_rep_name" varchar NOT NULL,
+  "customer_name" varchar NOT NULL,
+  "services_to_render" text[] NOT NULL DEFAULT '{ }',
+  "status" varchar NOT NULL DEFAULT 'ongoing',
+  "department" varchar NOT NULL,
+  "net_total_cost" numeric(11,2) NOT NULL,
+  "profit" numeric(11,2) NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT (now()),
+  "updated_at" timestamp NOT NULL DEFAULT ('0001-01-01 00:00:00'),
+  "closed_at" timestamp NOT NULL DEFAULT ('0001-01-01 00:00:00'),
+  "awarded" bool NOT NULL DEFAULT false
+);
+
+CREATE TABLE "pitch_requests" (
+  "id" bigserial PRIMARY KEY,
+  "user_id" bigint NOT NULL,
+  "customer_name" varchar NOT NULL,
+  "customer_request" varchar NOT NULL DEFAULT 'proposal',
+  "admin_task" varchar NOT NULL,
+  "admin_deadline" timestamp NOT NULL,
+  "admin_viewed" bool NOT NULL DEFAULT false,
+  "created_at" timestamp NOT NULL DEFAULT (now()),
+  "updated_at" timestamp NOT NULL DEFAULT ('0001-01-01 00:00:00')
+);
+
+ALTER TABLE "pitch_requests" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE;
+
+ALTER TABLE "deals" ADD FOREIGN KEY ("pitch_id") REFERENCES "pitch_requests" ("id") ON DELETE SET NULL;
