0cc2bae76e36501f4fb9808f64df3ce606e71fc3
Ebenezer <eebenezer949@gmail.com>
2024-10-01 23:06:50 +0100

    feat: add authorization middleware

---
+++ backend/middleware/middleware_utils.go
@@ -0,0 +1,14 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// abortHandlingRequest takes the ctx prevents handlers from being called
+// then sends json response to client
+func abortHandlingRequest(ctx *gin.Context, httpStatus int, errCode, errMsg string) {
+	ctx.AbortWithStatusJSON(httpStatus, gin.H{
+		"code":  errCode,
+		"error": errMsg,
+	})
+}
---
+++ backend/middleware/user_authorization.go
@@ -0,0 +1,50 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/ekefan/pre-sales-deal-tracker/backend/token"
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	authoHeaderKey       = "authorization"
+	authHeaderTypeBearer = "bearer"
+	authPayloadKey       = "auth_payload"
+)
+
+// UserAuthorization checks client requests to verify appropriate
+// authorization header is used and access token is still valid
+func UserAuthorization(tokenGenerator token.TokenGenerator) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		header := ctx.GetHeader("Authorization")
+
+		if len(header) == 0 {
+			abortHandlingRequest(ctx, http.StatusUnauthorized, "UNAUTHORIZED", "no authorization header passed")
+			return
+		}
+
+		headerFields := strings.Fields(header)
+		if len(headerFields) != 2 {
+			abortHandlingRequest(ctx, http.StatusUnauthorized, "UNAUTHORIZED", "invalid authorization header")
+			return
+		}
+
+		bearerField := strings.ToLower(headerFields[0])
+		if bearerField != authHeaderTypeBearer {
+			abortHandlingRequest(ctx, http.StatusUnauthorized, "UNAUTHORIZED", "unsupported authorization type")
+			return
+		}
+
+		accessToken := headerFields[1]
+		payload, err := tokenGenerator.VerifyToken(accessToken)
+		if err != nil {
+			abortHandlingRequest(ctx, http.StatusUnauthorized, "UNAUTHORIZED", "invalid access token")
+			return
+		}
+
+		ctx.Set(authPayloadKey, payload)
+		ctx.Next()
+	}
+}
