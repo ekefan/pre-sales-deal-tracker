509d639dec80beea9ce31227ff712dd0e6cf14e4
Ebenezer <eebenezer949@gmail.com>
2024-10-03 12:01:24 +0100

    api: handle login with correct responses

--- backend/api/auth_login.go
+++ backend/api/auth_login.go
@@ -2,12 +2,10 @@ package api
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 	"time"
 
-	// "time"
-
-	db "github.com/ekefan/pre-sales-deal-tracker/backend/db/sqlc"
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5"
 )
@@ -16,6 +14,18 @@ import (
 type LoginReq struct {
 	Username string `json:"username" binding:"required,alphanum,gte=4,lte=6"`
 	Password string `json:"password" binding:"required,alphanum"`
+}
+
+// UserLoginResp sub field in the login response body
+type UserLoginResp struct {
+	UserID          int64  `json:"user_id"`
+	Username        string `json:"username"`
+	FullName        string `json:"full_name"`
+	Role            string `json:"role"`
+	Email           string `json:"email"`
+	PasswordChanged bool   `json:"password_changed"`
+	UpdatedAt       string `json:"updated_at"`
+	CreatedAt       string `json:"created_at"`
 }
 
 // LoginResp holds the fields in the response body if login is successful
@@ -27,29 +37,47 @@ type LoginResp struct {
 // authLogin handles client log in
 func (server *Server) authLogin(ctx *gin.Context) {
 	var req LoginReq
-	if err := ctx.ShouldBindJSON(&req); err != nil {
-		ctx.JSON(http.StatusBadRequest, errorResponse(err, "BAD_REQUEST"))
+	if err := bindClientRequest(ctx, &req, jsonSource); err != nil {
 		return
 	}
 
 	user, err := server.store.GetUserByUsername(ctx, req.Username)
 	if err != nil {
+
 		if errors.Is(err, pgx.ErrNoRows) {
-			ctx.JSON(http.StatusNotFound, errorResponse(err, "NOT_FOUND"))
+			statusCode = http.StatusNotFound
+			errCode = "NOT_FOUND"
+			errMsg = "resouce not found"
+			details = fmt.Sprintf("no user with username %v exists", req.Username)
+			ctx.JSON(statusCode, errorResponse(statusCode, errCode, errMsg, details))
 			return
 		}
-		ctx.JSON(http.StatusInternalServerError, errorResponse(errors.New("can not connect to database"), "SERVER_ERROR"))
+		handleServerError(ctx, err)
 		return
 	}
 	if err := ValidatePassword(user.Password, req.Password); err != nil {
-		ctx.JSON(http.StatusUnauthorized, errorResponse(err, "INVALID_PASSWORD"))
+		statusCode = http.StatusUnauthorized
+		errCode = "UNAUTHORIZED"
+		errMsg = "invalid password"
+		details = "the user password sent by this client is incorrect"
+		ctx.JSON(statusCode, errorResponse(statusCode, errCode, errMsg, details))
 		return
 	}
 	accessToken, _, err := server.tokenGenerator.GenerateToken(user.ID, user.Role, server.config.TokenDuration)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err, "SEVER_ERROR"))
+		handleServerError(ctx, err)
+		return
 	}
-	userData := generateUserData(user)
+	userData := UserLoginResp{
+		UserID:          user.ID,
+		Username:        user.Username,
+		FullName:        user.FullName,
+		Role:            user.Role,
+		Email:           user.Email,
+		PasswordChanged: user.PasswordChanged,
+		UpdatedAt:       user.UpdatedAt.Time.Format(time.RFC3339),
+		CreatedAt:       user.CreatedAt.Time.Format(time.RFC3339),
+	}
 
 	resp := LoginResp{
 		AccessToken: accessToken,
@@ -57,33 +85,4 @@ func (server *Server) authLogin(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, resp)
-}
-
-// generateUserData generates a userLoginResp struct
-// updatedAt field will be nil if user has never been updated
-func generateUserData(user db.User) UserLoginResp {
-
-	if !user.UpdatedAt.Time.IsZero() {
-		return UserLoginResp{
-			UserID:          user.ID,
-			Username:        user.Username,
-			Fullname:        user.FullName,
-			Role:            user.Role,
-			Email:           user.Email,
-			PasswordChanged: user.PasswordChanged,
-			UpdatedAt:       nil,
-		}
-	}
-
-	updatedAt := user.UpdatedAt.Time.Format(time.RFC3339)
-
-	return UserLoginResp{
-		UserID:          user.ID,
-		Username:        user.Username,
-		Fullname:        user.FullName,
-		Role:            user.Role,
-		Email:           user.Email,
-		PasswordChanged: user.PasswordChanged,
-		UpdatedAt:       &updatedAt,
-	}
 }
