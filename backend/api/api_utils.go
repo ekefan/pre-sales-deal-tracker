70e24f29db9f3ef63b8d2dcb7d7864366ed90ad6
Ebenezer <eebenezer949@gmail.com>
2024-10-06 22:43:26 +0100

    api: refactor code

--- backend/api/api_utils.go
+++ backend/api/api_utils.go
@@ -1,58 +1,12 @@
 package api
 
 import (
-	"errors"
-	"fmt"
-	"log/slog"
-	"net/http"
-	"strings"
 	"time"
 
-	"github.com/ekefan/pre-sales-deal-tracker/backend/middleware"
-	"github.com/ekefan/pre-sales-deal-tracker/backend/token"
 	"github.com/gin-gonic/gin"
-	"github.com/jackc/pgerrcode"
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/spf13/viper"
 	"golang.org/x/crypto/bcrypt"
 )
-
-// variables for designing error responses
-var (
-	statusCode               int
-	errCode, errMsg, details string
-)
-
-// errorResponse sends custom error response to client
-// with code: a sentinel error eg. NOT_FOUND and err the error
-// FIXME: create a struct exposed to hold the error. Then, create a "constructor" function to instantiate it.
-func errorResponse(statusCode int, code, message, details string) gin.H {
-	errResp := struct {
-		Code    string `json:"code"`
-		Message string `json:"message"`
-		Details string `json:"details"`
-	}{
-		Code:    code,
-		Message: message,
-		Details: details,
-	}
-	return gin.H{
-		// FIXME: it's redundant. We already have this information in the response Status Code Header.
-		"status_code": statusCode,
-		"error":       errResp,
-	}
-}
-
-// handleServerError snipet for returning server error to the client
-func handleServerError(ctx *gin.Context, err error) {
-	statusCode = http.StatusInternalServerError
-	errCode = "SERVER_ERROR"
-	errMsg = "a server error"
-	details = "server failed to while handling request"
-	slog.Error(err.Error())
-	ctx.JSON(statusCode, errorResponse(statusCode, errCode, errMsg, details))
-}
 
 // successMessage sends a custom success response to client
 func successMessage() gin.H {
@@ -69,32 +23,19 @@ const (
 
 // bindClientRequest binds client request to req, which must always be a pointer to the request struct
 // the binding source represents the kind of binding to perform on the request
-// FIXME: here you're doing too many things: bind the client request and setting the response payload.
+// Fixme: here you're doing too many things: bind the client request and setting the response payload.
+// Done: binding the request only
 func bindClientRequest(ctx *gin.Context, req any, bindingSource int) error {
-	statusCode := http.StatusBadRequest
-	errCode := "BAD_REQUEST"
-	errMsg := "failed to bind client request"
-	details := "invalid request params have been sent, make sure request params are valid"
+	var err error
 	switch bindingSource {
 	case uriSource:
-		if err := ctx.ShouldBindUri(req); err != nil {
-			ctx.JSON(statusCode, errorResponse(statusCode, errCode, errMsg, details))
-			return err
-		}
+		err = ctx.ShouldBindUri(req)
 	case querySource:
-		if err := ctx.ShouldBindQuery(req); err != nil {
-			ctx.JSON(statusCode, errorResponse(statusCode, errCode, errMsg, details))
-			return err
-		}
+		err = ctx.ShouldBindQuery(req)
 	case bindingSource:
-		if err := ctx.ShouldBindJSON(req); err != nil {
-			ctx.JSON(statusCode, errorResponse(statusCode, errCode, errMsg, details))
-			return err
-		}
-	default:
-		return errors.New("bindingSource not supported")
+		err = ctx.ShouldBindJSON(req)
 	}
-	return nil
+	return err
 }
 
 // HashPassword uses bcrypt to generate a hash from password
@@ -140,29 +81,6 @@ func ReadConfigFiles(configPath string) (*Config, error) {
 	return &config, nil
 }
 
-const (
-	adminRole   = "admin"
-	salesRole   = "sales"
-	managerRole = "manager"
-)
-
-// authAccess ensures the request is from a user with role in roles
-func authAccess(ctx *gin.Context, roles []string) bool {
-	payload := ctx.MustGet(middleware.AuthPayloadKey).(*token.Payload)
-	for _, role := range roles {
-		if role == payload.Role {
-			return true
-		}
-	}
-	errMsg := "user not authorized to access resource"
-	errCode := "FORBIDDEN"
-	errDetail := fmt.Sprintf("resource can only be accessed by user with such role: %v", strings.Join(roles, ", "))
-	statusCode := http.StatusForbidden
-	ctx.JSON(statusCode,
-		errorResponse(statusCode, errCode, errMsg, errDetail))
-	return false
-}
-
 // Pagination holds pagination data for users resource
 type Pagination struct {
 	TotalRecords int32 `json:"total_records"`
@@ -188,27 +106,4 @@ func generatePagination(totalRecords, pageID, pageSize int32) Pagination {
 		HasNext:      totalPages > pageID,
 		HasPrevious:  pageID > 1,
 	}
-}
-
-// pgxError handles database error, returns true if error is a pgxError
-// else returns false
-func pgxError(ctx *gin.Context, err error, msg, errDetails string) bool {
-	var pgErr *pgconn.PgError
-	if errors.As(err, &pgErr) && pgErr.Code == pgerrcode.UniqueViolation {
-		statusCode = http.StatusConflict
-		errCode = "STATUS_CONFLICT"
-		errMsg = msg
-		details = errDetails
-		ctx.JSON(statusCode, errorResponse(statusCode, errCode, errMsg, details))
-		return true
-	}
-	if errors.Is(err, pgx.ErrNoRows) {
-		statusCode = http.StatusNotFound
-		errCode = "NOT_FOUND"
-		errMsg = msg
-		details = errDetails
-		ctx.JSON(statusCode, errorResponse(statusCode, errCode, errMsg, details))
-		return true
-	}
-	return false
 }
