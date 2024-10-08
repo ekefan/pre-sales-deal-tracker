30f6b405e027a22e155ef6dc966269e1a721ed46
Ebenezer <eebenezer949@gmail.com>
2024-10-08 14:34:42 +0100

    api: abstract api models from database models

--- backend/api/api_models.go
+++ backend/api/api_models.go
@@ -10,3 +10,30 @@ type User struct {
 	UpdatedAt       string `json:"updated_at"`
 	CreatedAt       string `json:"created_at"`
 }
+
+type Deal struct {
+	DealID           int64    `json:"deal_id"`
+	PitchId          int64    `json:"pitch_id"`
+	SalesRepName     string   `json:"sales_rep_name"`
+	CustomerName     string   `json:"customer_name"`
+	ServicesToRender []string `json:"services_to_render"`
+	Status           string   `json:"status"`
+	Department       string   `json:"department"`
+	NetTotalCOst     float64  `json:"net_total_cost"`
+	Profit           float64  `json:"profit"`
+	Awarded          bool     `json:"awarded"`
+	UpdatedAt        string   `json:"updated_at"`
+	CreatedAt        string   `json:"created_at"`
+}
+
+type PitchRequest struct {
+	PitchID         int64    `json:"pitch_id"`
+	UserID          int64    `json:"user_id"`
+	CustomerName    string   `json:"customer_name"`
+	CustomerRequest []string `json:"customer_request"`
+	AdminTask       string   `json:"admin_task"`
+	AdminDeadline   string   `json:"admin_deadline"`
+	AdminViewed     bool     `json:"admin_viewed"`
+	UpdatedAt       string   `json:"updated_at"`
+	CreatedAt       string   `json:"created_at"`
+}
